6294b4d21ada044228bb44b9cce26226959b2b15
luklue <luklue@users.noreply.github.com>
2015-10-02 14:33:46 +0200

    Create firststart.sh

---
+++ temperatureEvaluation/firststart.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/readings
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/activeTrueFalse.py
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/checkHardwareButton.py
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/checkLed.sh
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/firststart.sh
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/projectname.json
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/readTemperature.py
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/sensornames.json
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/start.sh
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/startButton
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/stopButton
+chmod o+rwx /var/www/temperature-monitoring/temperatureEvaluation/writeSensorNames.py
+
+fi
